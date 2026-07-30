18cf86ed01e1c07953cc3b8314a6357fe29efa28
Hamza Hameed Niazi <hamzahameedniazi8989@gmail.com>
2026-07-30 22:32:51 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -10,9 +10,8 @@ export const clearToken = () => localStorage.removeItem(TOKEN_KEY);
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.DEV
-    ? "http://localhost:3000/api"
-    : "/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
  
 
