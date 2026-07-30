ffb020f21153b478738bf5033bec02c4a93dc31d
Hamza Hameed Niazi <hamzahameedniazi8989@gmail.com>
2026-07-30 22:28:15 +0530

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { getToken } from "./api";
 
-const SOCKET_URL = import.meta.env.DEV
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL
   ? "http://localhost:3000"
   : "";
 
